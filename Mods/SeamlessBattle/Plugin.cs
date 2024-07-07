a507562e5fe46b5d49f0cbc0a06711109ea43204
poyoyuoh <155378590+poyoyuoh@users.noreply.github.com>
2024-07-07 19:12:37 +0200

    Add SeamlessBattle

---
+++ Mods/SeamlessBattle/Plugin.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace SeamlessBattle;
+
+[BepInPlugin(GUID, PluginName, PluginVersion)]
+[BepInProcess("Digimon World Next Order.exe")]
+public class Plugin : BasePlugin
+{
+    internal const string GUID = "poyoyuoh.DWNO.SeamlessBattle";
+    internal const string PluginName = "SeamlessBattle";
+    internal const string PluginVersion = "1.0.0";
+
+    public override void Load() => Harmony.CreateAndPatchAll(typeof(Plugin));
+
+    [HarmonyPatch(typeof(uBattlePanel), "Update")]
+    [HarmonyPostfix]
+    public static void uBattlePanel_Update_Postfix(uBattlePanel __instance)
+    {
+        if (__instance.m_isTactics)
+            __instance.DrawTargetArrow();
+        
+        if (__instance.m_isOpenCmd)
+            MainGameManager.Ref.TimeScale = 1f;
+    }
+
+    [HarmonyPatch(typeof(uBattlePanel), "EnableTacticsMode")]
+    [HarmonyPostfix]
+    public static void uBattlePanel_EnableTacticsMode_Postfix(uBattlePanel __instance)
+    {
+        MainGameManager.Ref.TimeScale = 1f;
+        __instance.VisibleDamage = true;
+    }
+
+    [HarmonyPatch(typeof(uBattlePanel), "EnableItemBoxMode")]
+    [HarmonyPostfix]
+    public static void uBattlePanel_EnableItemBoxMode_Postfix(uBattlePanel __instance)
+    {
+        MainGameManager.Ref.TimeScale = 1f;
+        __instance.VisibleDamage = true;
+    }
+
+    [HarmonyPatch(typeof(uBattlePanelCommand), "CommandExecution")]
+    [HarmonyPostfix]
+    public static void uBattlePanelCommand_CommandExecution_Postfix()
+    {
+        MainGameManager.Ref.TimeScale = 1f;
+    }
+}
