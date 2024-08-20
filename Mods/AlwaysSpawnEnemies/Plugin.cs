deb925b4b767298bf1dc2dea471d4a7664f0a9c1
poyoyuoh <155378590+poyoyuoh@users.noreply.github.com>
2024-08-20 19:12:11 +0200

    Add AlwaysSpawnEnemies

---
+++ Mods/AlwaysSpawnEnemies/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace AlwaysSpawnEnemies;
+
+[BepInPlugin(GUID, PluginName, PluginVersion)]
+[BepInProcess("Digimon World Next Order.exe")]
+public class Plugin : BasePlugin
+{
+    internal const string GUID = "poyoyuoh.DWNO.AlwaysSpawnEnemies";
+    internal const string PluginName = "AlwaysSpawnEnemies";
+    internal const string PluginVersion = "1.0.0";
+
+    public override void Load() => Harmony.CreateAndPatchAll(typeof(Plugin));
+
+    [HarmonyPatch(typeof(EnemyManager), "_IsCreateNormalEnamyIncidence")]
+    [HarmonyPrefix]
+    public static bool EnemyManager__IsCreateNormalEnamyIncidence_Prefix(ref bool __result)
+    {
+        __result = true;
+        return false;
+    }
+}
